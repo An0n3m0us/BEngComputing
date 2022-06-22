fe9a7c56006f1e87a0e8299302ea73d04209a8de
An0n3m0us <an0n3m0us@hotmail.com>
2022-06-22 01:55:08 +0100

    Simplified a little

--- ComputationalMathematics/NewtonDiff.jl
+++ ComputationalMathematics/NewtonDiff.jl
@@ -10,16 +10,18 @@ function newtonDifference(x, xk, yk)
     if !(x >= xk[1] && x <= xk[end]) return end
 
     # Forward difference is the precendence
-    N = x <= xkMid ? 0 : 1
+    N = !(x <= xkMid)
+
     # Loop through xk values
     for k = 1:length(xk)-1
-        # y[k+1-N] - y[k-N] gives (y[k+1] - y[k]) or (y[k] - y[k-1]) for FDiff or BDiff respectively
-        diff = [calcDiff(dnyk[end][i+1-N], dnyk[end][i-N]) for i in (1+N):length(dnyk[end])+(-1+N)]
-        # Columnize so in correct format and fill with NaN depending of FDiff or BDiff
-        columnize = Bool(N) ? vcat([NaN], diff) : vcat(diff, [NaN])
-        push!(dnyk, columnize)
+        # y[k+1-N] - y[k-N] gives (y[k+1] - y[k]) or (y[k] - y[k-1])
+        # for FDiff (N = 0) or BDiff (N = 1) respectively
+        diff = vcat(repeat([NaN], N), [calcDiff(dnyk[end][i+1-N], dnyk[end][i-N]) for i in (1+N):length(dnyk[end])+(-1+N)], repeat([NaN], !N))
+        push!(dnyk, diff)
     end
-    return [i-1 for i in 1:length(xk)], dnyk
+
+    # Return k and dnyk
+    return [k-1 for k in 1:length(xk)], dnyk
 end
 
 x = 1.5
@@ -27,4 +29,5 @@ xk = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
 yk = [0.000, -0.189, -0.317, -0.332, -0.200, 0.091, 0.525, 1.065, 1.658, 2.243, 2.757]
 
 k, dnyk = newtonDifference(x, xk, yk)
+@show k
 [@show i for i in dnyk]
